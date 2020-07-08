e0b4ede2befccb3137336c1635cc4cbb68f70fbc
mileine <43147648+mileine@users.noreply.github.com>
2020-07-08 02:18:06 -0300

    Update sections

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Document by Code Bushi`,
-    name: `Code Bushi`,
-    siteUrl: `https://gatsby-theme-document.netlify.com`,
+    title: `Frontend Learning`,
+    name: `Mileine`,
+    siteUrl: `https://frontend-learning.netlify.app/`,
     description: `This is my description that will be used in the meta tags and important for search results`,
     social: [
       {
